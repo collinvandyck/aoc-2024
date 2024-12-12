c662741db76cb3f9b2a0860c3d9c477753339f01
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-12 00:27:12 -0500

    wip

---
+++ src/bin/12.rs
@@ -0,0 +1,24 @@
+#![allow(unused)]
+
+#[cfg(test)]
+static EX1: &str = include_str!("../../data/12/ex1");
+static IN1: &str = include_str!("../../data/12/in1");
+
+fn main() {
+    println!("pt1: {}", aoc_2024::timed(|| eval(IN1, true)));
+    println!("pt2: {}", aoc_2024::timed(|| eval(IN1, false)));
+}
+
+fn eval(s: &str, pt1: bool) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exs() {
+        assert_eq!(eval(EX1, true), 55312);
+    }
+}
