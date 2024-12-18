60ab19c574733c0896c91804525dbab22fbf2748
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-18 11:46:24 -0500

    wip

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -16,26 +16,41 @@ fn main() {
 fn eval(s: &str, ex: bool, pt1: bool) -> usize {
     let bytes = parse(s);
     let dim = ex.then_some(7).unwrap_or(71);
-    let mut grid = Grid { dim, bytes };
+    let mut grid = Grid::new(dim, bytes);
     grid.solve()
 }
 
 struct Grid {
     dim: i32,
-    bytes: Vec<(i32, i32)>,
+    bytes: Vec<Pos>,
+    runners: Vec<Runner>,
 }
+
+#[derive(Default)]
+struct Runner {
+    pos: Pos,
+}
+
+#[derive(Default)]
+struct Pos(i32, i32);
 
 impl Grid {
     fn solve(&mut self) -> usize {
         todo!("solve")
     }
+    fn new(dim: i32, bytes: Vec<Pos>) -> Self {
+        let runner = Runner::default();
+        let runners = vec![runner];
+        Self { dim, bytes, runners }
+    }
 }
 
-fn parse(s: &str) -> Vec<(i32, i32)> {
+fn parse(s: &str) -> Vec<Pos> {
     s.trim()
         .lines()
         .flat_map(|l| l.split_once(","))
-        .map(|f| (f.0.parse().unwrap(), f.1.parse().unwrap()))
+        .map(|t| (t.0.parse().unwrap(), t.1.parse().unwrap()))
+        .map(|t| Pos(t.0, t.1))
         .collect()
 }
 
