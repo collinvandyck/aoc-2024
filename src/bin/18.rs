adf1dc243964ddc8412a50bf07c66470403ae325
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-18 11:28:23 -0500

    wip

---
+++ src/bin/18.rs
@@ -0,0 +1,28 @@
+#![allow(unused)]
+
+use itertools::Itertools;
+
+#[allow(unused)]
+mod data {
+    pub static EX1: &str = include_str!("../../data/17/ex1");
+    pub static IN1: &str = include_str!("../../data/17/in1");
+}
+
+fn main() {
+    println!("pt1: {}", aoc_2024::timed(|| eval(data::IN1, true)));
+    // println!("pt2: {}", aoc_2024::timed(|| eval(data::IN1, false)));
+}
+
+fn eval(s: &str, pt1: bool) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ex1() {
+        assert_eq!(eval(data::EX1, true), 22);
+    }
+}
