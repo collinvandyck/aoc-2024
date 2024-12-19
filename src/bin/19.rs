ef5fc6f5d72562b6ac9360a8bd6f98d863dc0f29
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-19 17:21:51 -0500

    wip

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -12,7 +12,41 @@ fn main() {
 }
 
 fn eval(s: &str, pt1: bool) -> usize {
+    let prob = parse(s);
+    println!("{prob:#?}");
     todo!("eval")
+}
+
+#[derive(Clone)]
+struct Colors(Vec<u8>);
+
+impl FromIterator<u8> for Colors {
+    fn from_iter<T: IntoIterator<Item = u8>>(iter: T) -> Self {
+        Colors(iter.into_iter().collect())
+    }
+}
+
+impl std::fmt::Debug for Colors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = std::str::from_utf8(&self.0).unwrap();
+        write!(f, "{s}")
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Problem {
+    patterns: Vec<Colors>,
+    designs: Vec<Colors>,
+}
+
+fn parse(s: &str) -> Problem {
+    let (p1, p2) = s.trim().split_once("\n\n").unwrap();
+    let patterns = p1
+        .split(", ")
+        .map(|s| s.bytes().collect())
+        .collect();
+    let designs = p2.lines().map(|s| s.bytes().collect()).collect();
+    Problem { patterns, designs }
 }
 
 #[cfg(test)]
