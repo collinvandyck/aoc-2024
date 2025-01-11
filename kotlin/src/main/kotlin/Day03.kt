65e2de73d1d39f16725cc37da67a807ea3118e05
Collin Van Dyck <collinvandyck@gmail.com>
2025-01-11 00:54:22 -0500

    wip

---
+++ kotlin/src/main/kotlin/Day03.kt
@@ -0,0 +1,19 @@
+package day03
+
+import utils.readFixture
+import utils.timed
+
+fun main() {
+    utils.init()
+    val in1 = readFixture("03/in1")
+    println("pt1: ${timed { pt1(in1) }}")
+    println("pt2: ${timed { pt2(in1) }}")
+}
+
+fun pt1(s: String): Int {
+    TODO()
+}
+
+fun pt2(s: String): Int {
+    TODO()
+}
