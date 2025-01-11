ba2df026bc8eb6d7f41ca8197098fa7d06b4ca5d
Collin Van Dyck <collinvandyck@gmail.com>
2025-01-11 01:21:51 -0500

    Day 03 Kotlin.

--- kotlin/src/main/kotlin/Day03.kt
+++ kotlin/src/main/kotlin/Day03.kt
@@ -11,9 +11,27 @@ fun main() {
 }
 
 fun pt1(s: String): Int {
-    TODO()
+    val re = Regex("""mul\((\d+),(\d+)\)""")
+    return re.findAll(s).map {
+        val (a, b) = it.groupValues[1] to it.groupValues[2]
+        a.toInt() * b.toInt()
+    }.sum()
 }
 
 fun pt2(s: String): Int {
-    TODO()
+    data class State(val ok: Boolean = true, val sum: Int = 0)
+
+    val re = Regex("""do\(\)|don't\(\)|mul\((\d+),(\d+)\)""")
+    return re.findAll(s).fold(State(true, 0)) { (ok, sum), xs ->
+        when (xs.groupValues[0]) {
+            "do()" -> State(true, sum)
+            "don't()" -> State(false, sum)
+            else -> {
+                if (ok) {
+                    val (a, b) = xs.groupValues[1] to xs.groupValues[2]
+                    State(ok, sum + (a.toInt() * b.toInt()))
+                } else State(ok, sum)
+            }
+        }
+    }.sum
 }
--- kotlin/src/test/kotlin/Day03Test.kt
+++ kotlin/src/test/kotlin/Day03Test.kt
@@ -7,13 +7,13 @@ class Day03Test {
     @Test
     fun testPt1() {
         val ex1 = readFixture("03/in1")
-        assertEquals(42, day03.pt1(ex1))
+        assertEquals(185797128, day03.pt1(ex1))
     }
 
     @Test
     fun testPt2() {
         val ex1 = readFixture("03/in1")
-        assertEquals(42, day03.pt2(ex1))
+        assertEquals(89798695, day03.pt2(ex1))
     }
 }
 
