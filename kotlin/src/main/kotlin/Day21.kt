e57fda31c7d6deafdaf050c55446413edf2ea63b
Collin Van Dyck <collin@5xx.engineer>
2025-01-30 13:48:52 -0500

    wip

--- kotlin/src/main/kotlin/Day21.kt
+++ kotlin/src/main/kotlin/Day21.kt
@@ -1,11 +1,14 @@
+@file:Suppress("unused", "UNUSED_PARAMETER")
+
 package day21
 
 import utils.readFixture
 import utils.timed
+import kotlin.math.max
 
 fun main() {
     val in1 = readFixture("21/in1")
-    repeat(10) {
+    repeat(1) {
         println("pt1: ${timed { pt1(in1) }}")
         println("pt2: ${timed { pt2(in1) }}")
     }
@@ -15,11 +18,35 @@ fun pt1(s: String) = Code.parse(s).take(1).sumOf { it.complexity() }
 
 fun pt2(s: String): Int = TODO()
 
+sealed class Key
+data class Move(val from: Tile, val to: Tile, val dir: Dir) : Key() {
+    constructor(from: Tile, to: Tile) : this(from, to, from.dirTo(to))
+
+    override fun toString() = "{${from.ch}->${to.ch}}"
+}
+
+data object Press : Key()
+
 data class Code(val chars: List<Char>) {
     fun shortestSequence(): Int {
+        println("code: $chars")
         val numKey = NumKey()
-        val paths = numKey.paths(numKey.mustGet('A'), numKey.mustGet('5'), shortest = true)
-        println("paths:\n${paths.joinToString("\n")}")
+        val chs = listOf(numKey.pos.ch) + chars
+        val paths = chs.windowed(2)
+            .map { (ch1, ch2) ->
+                val (tSrc, tDst) = listOf(ch1, ch2).map { numKey.mustGet(it) }
+                numKey.paths(tSrc, tDst)
+            }
+            .fold(listOf(listOf<Tile>())) { acc, list ->
+                // TODO: convert each list in lists to Keys and add presses
+                println("list: $list")
+                acc.flatMap { pl ->
+                    list.map { nl -> pl.take(max(0, pl.size - 1)) + nl }
+                }
+            }
+        for (path in paths) {
+            println(path)
+        }
         return 0
     }
 
@@ -44,7 +71,7 @@ class NumKey : Graph("789\n456\n123\nX0A")
 class DirKey : Graph("X^A\n<v>")
 
 open class Graph(chs: String) {
-    data class Key(val start: Tile, val end: Tile, val shortest: Boolean)
+    data class PathsKey(val start: Tile, val end: Tile, val shortest: Boolean)
 
     val tiles: List<List<Tile>> = chs.trim().lines()
         .mapIndexed { row, line ->
@@ -53,12 +80,11 @@ open class Graph(chs: String) {
                 Tile(pt, ch, this)
             }
         }
-
     val pos: Tile = tiles.flatten().find { it.ch == 'A' } ?: error("no start")
-    val pathCache = mutableMapOf<Key, List<List<Tile>>>()
+    val pathCache = mutableMapOf<PathsKey, List<List<Tile>>>()
 
     fun paths(start: Tile, end: Tile, shortest: Boolean = true): List<List<Tile>> {
-        val paths = pathCache.getOrElse(Key(start, end, shortest), {
+        val paths = pathCache.getOrElse(PathsKey(start, end, shortest)) {
             val paths: MutableList<List<Tile>> = mutableListOf()
             val addBest = fun(path: List<Tile>) {
                 if (!shortest) {
@@ -91,10 +117,10 @@ open class Graph(chs: String) {
                     queue.addLast(ns.also { it.add(next) })
                 }
             }
-            pathCache[Key(start, end, shortest)] = paths
-            pathCache[Key(end, start, shortest)] = paths.map { it.reversed() }
+            pathCache[PathsKey(start, end, shortest)] = paths
+            pathCache[PathsKey(end, start, shortest)] = paths.map { it.reversed() }
             paths
-        })
+        }
         return paths
     }
 
@@ -111,7 +137,14 @@ open class Graph(chs: String) {
 }
 
 data class Tile(val pt: Point, val ch: Char, val graph: Graph) {
-    override fun toString() = "$pt:$ch"
+    override fun toString() = "T{$pt:$ch}"
+    fun dirTo(to: Tile): Dir = when {
+        pt.row < to.pt.row -> Dir.D
+        pt.row > to.pt.row -> Dir.U
+        pt.col > to.pt.col -> Dir.L
+        pt.col < to.pt.col -> Dir.R
+        else -> error("invalid dirTo from:$this to:$this")
+    }
 }
 
 data class Point(val row: Int, val col: Int) {
